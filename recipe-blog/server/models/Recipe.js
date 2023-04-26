7159a50af83f6964eb4608768d54a08b5f0cdeaf
Antonis <107072477+vlantonakos@users.noreply.github.com>
2023-04-26 13:41:54 +0300

    Add files via upload

---
+++ recipe-blog/server/models/Recipe.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const recipeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: 'This fild is required.'
+    },
+    description: {
+        type: String,
+        required: 'This fild is required.'
+    },
+    email: {
+        type: String,
+        required: 'This fild is required.'
+    },
+    ingredients: {
+        type: Array,
+        required: 'This fild is required.'
+    },
+    category : {
+        type: String,
+        enum: ['Thai','American','Chinese','Mexican','Indian','Spanish'],
+        required: 'This fild is required.'
+    },
+    image: {
+        type: String,
+        required: 'This fild is required.'
+    }
+})
+
+recipeSchema.index({ name: 'text', description: 'text' });
+
+module.exports = mongoose.model('Recipe', recipeSchema)
---
+++ recipe-blog/server/routes/recipeRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const recipeController = require('../controllers/recipeController')
+
+// App Routes
+router.get('/',recipeController.homepage)
+router.get('/categories',recipeController.exploreCategories)
+router.get('/categories/:id',recipeController.exploreCategoriesByID)
+router.get('/recipe/:id', recipeController.exploreRecipe)
+router.post('/search',recipeController.searchRecipe)
+router.get('/explore-latest',recipeController.exploreLatest)
+router.get('/explore-random',recipeController.exploreRandom)
+
+module.exports = router
